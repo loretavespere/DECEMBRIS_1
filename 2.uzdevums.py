397a61ac5bd34d9dd614b997100a688ea3cbc93f
loretavespere <loreta.vespere@grobinasskola.lv>
2023-11-16 10:18:25 +0200

    uzdevumi

---
+++ 2.uzdevums.py
@@ -0,0 +1,12 @@
+text=input ("Ievadi tekstu!")
+skaititajs=0
+for burts in text:
+    if burts =='a':
+        skaititajs +=1
+        print("Burtu a skaits=",skaititajs)
+        len 
+        sar1=[5,46,1,0,45,10]
+        garums=len(sar1)
+        print(garums)
+        lielaks=max(sar1)
+        print(lielaks)
