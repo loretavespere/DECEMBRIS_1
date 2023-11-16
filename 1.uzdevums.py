7e72bc8da9a93c7c04487b57ed1fa06c9cc281fa
loretavespere <loreta.vespere@grobinasskola.lv>
2023-11-16 10:08:54 +0200

    1.uzdevums

---
+++ 1.uzdevums.py
@@ -0,0 +1,6 @@
+text=input("Ievadi tekstu!")
+skaititajs=0
+for burts in text:
+    if burts =='a': 
+        skaititajs +=1
+print("Burtu a skaits=", skaititajs)
---
+++ 3.uzdevums.py
@@ -0,0 +1,5 @@
+skait=0
+skait+=1
+While <=100:
+if skait%2!=0:
+    print(skait)
